b10290aa120294a68b44027eb635d649ee84afd7
Basile <sbr0574@gmail.com>
2026-08-06 11:31:16 +0800

    更新主文件头部注释

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-# python3 -m PyInstaller -F -w .\main.py --name StockWidget --icon .\resources\StockWidget.ico
+
+# -*- coding: utf-8 -*-
+"""
+StockWidget (Project by @sbr0574)
+极简透明盯盘 Widget 浮窗, 按指定股票代码实时显示行情表格
+
+Repository: https://github.com/sbr0574/StockWidget
+Update  : 2026-08-06
+Version : 1.3.0
+License : Apache-2.0 license
+"""
+
 import sys
 import platform
 from src.App import App, APP_NAME
