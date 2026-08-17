a9996c9bbd2108c2d846b943972d98cb50fd392f
Basile <sbr0574@gmail.com>
2026-08-17 21:52:06 +0800

    feat(version): 更新应用版本至1.4.0

--- stockwidget/constants.py
+++ stockwidget/constants.py
@@ -2,7 +2,7 @@
 """全局常量：应用名/版本、配置文件、市场代码列表文件与下载地址。"""
 
 APP_NAME = "StockWidget"
-APP_VERSION = "1.3.1"
+APP_VERSION = "1.4.0"
 CONFIG_FILE = "stock_widget_config.json"
 
 # 全市场代码列表：三个独立 JSON（GitHub Action 每日更新后由程序下载）
